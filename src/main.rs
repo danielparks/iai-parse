166083404015d8652cae1c000d1e7135390c5542
Daniel Parks <dp+git@oxidized.org>
2023-01-17 17:00:04 -0800

    WIP: --git-revs.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 use anyhow::Context;
 use clap::Parser;
+use git2::Repository;
 use std::convert::From;
 use std::fs;
 use std::io;
 use std::path::{Path, PathBuf};
 use std::process::exit;
+use std::result::Result;
 
 #[derive(Debug, clap::Parser)]
 #[clap(version, about)]
 struct Params {
     /// File(s) to parse.
     input: Vec<PathBuf>,
+
+    /// Git revisions to check, e.g. main..HEAD.
+    #[clap(long, short('r'), value_name = "REVSPEC")]
+    git_revs: Vec<String>,
 }
 
 struct Receiver<W: io::Write> {
@@ -51,6 +57,21 @@ fn main() {
 }
 
 fn cli(params: Params) -> anyhow::Result<()> {
+    if !params.git_revs.is_empty() {
+        let repo = Repository::open_from_env()?;
+        for revspec_str in params.git_revs {
+            for commit in revspec_parse(&repo, &revspec_str)? {
+                let commit = commit?;
+                println!(
+                    "{} {}",
+                    abbrev(commit.id()),
+                    commit.summary().unwrap_or("")
+                );
+            }
+        }
+        return Ok(());
+    }
+
     let mut writer = csv::Writer::from_writer(io::stdout());
     writer.write_record([
         &b"benchmark"[..],
@@ -65,6 +86,58 @@ fn cli(params: Params) -> anyhow::Result<()> {
     }
 
     Ok(())
+}
+
+// FIXME: should we be checking that the abbreviations are unique?
+fn abbrev(oid: git2::Oid) -> String {
+    let mut hash = oid.to_string();
+    hash.truncate(7);
+    hash
+}
+
+fn revspec_parse<'r>(
+    repo: &'r Repository,
+    revspec_str: &str,
+) -> anyhow::Result<
+    Box<dyn Iterator<Item = Result<git2::Commit<'r>, git2::Error>> + 'r>,
+> {
+    let revspec = repo.revparse(revspec_str)?;
+    match (revspec.from(), revspec.to()) {
+        (None, None) => {
+            anyhow::bail!("Got no revisions from revspec {revspec_str:?}");
+        }
+        (None, Some(_to)) => {
+            anyhow::bail!(
+                "Unsure how to handle revspec with only .to(): {revspec_str:?}"
+            );
+        }
+        (Some(from), None) => {
+            // Single revision.
+            let mut walker = repo.revwalk()?;
+            walker.push(from.id())?;
+            Ok(Box::new(walker.take(1).map(|oid_result| {
+                oid_result.and_then(|oid| repo.find_commit(oid))
+            })))
+        }
+        (Some(from), Some(to)) => {
+            // Range of revisions.
+            let mut walker = repo.revwalk()?;
+            walker.push(to.id())?;
+            let from_oid = from.id();
+            Ok(Box::new(walker.map_while(move |oid_result| {
+                match oid_result {
+                    Ok(oid) => {
+                        if oid == from_oid {
+                            None // Stop iterating
+                        } else {
+                            Some(repo.find_commit(oid))
+                        }
+                    }
+                    Err(error) => Some(Err(error)),
+                }
+            })))
+        }
+    }
 }
 
 fn read<P: AsRef<Path>>(path: P) -> anyhow::Result<Vec<u8>> {
