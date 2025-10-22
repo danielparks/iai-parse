522b0ce55147c961461a7e72e2ac21cd141d10a9
Daniel Parks <dp+git@oxidized.org>
2025-10-22 12:53:34 -0700

    `release.sh`: re-enable `cargo publish`.

--- release.sh
+++ release.sh
@@ -138,7 +138,7 @@ git tag --force --sign --file "$changelog" --cleanup=verbatim "v${version}"
 git push --force --tags origin HEAD
 auto-pr "Release ${version}"
 
-#cargo publish
+cargo publish
 
 awk-in-place CHANGELOG.md '
   /^## Release/ && !done {
